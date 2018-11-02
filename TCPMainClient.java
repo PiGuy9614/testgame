4a09d42eb898564f46e720370cbeb1f491684dc4
Nicholas Koeppen <nicholaskoeppen@gmail.com>
2018-11-02 17:10:21 -0500

    starting rock paper scissors development

--- TCPMainClient.java
+++ TCPMainClient.java
@@ -6,20 +6,24 @@ import javax.swing.*;
 class TCPMainClient {
     public static void main(String argv[]) throws Exception {
         String clientSentence;
-        String capitalizedSentence;
         ServerSocket welcomeSocket = new ServerSocket(9614);
         while (true) {
-            Socket connectionSocket = welcomeSocket.accept();
-            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            Socket connection = welcomeSocket.accept();
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connection.getOutputStream());
 
-            InetAddress client = connectionSocket.getInetAddress(); 
-            String clientName = inFromClient.readLine();;
+            InetAddress client = connection.getInetAddress(); 
+            String clientName = inFromClient.readLine();
 
             outToClient.writeBytes(JOPI("Please tell " + clientName + " your username?"));
+            
+            String mainClientmove = JOPI("You are playing with " + clientName + ". What is your move?");
+            String clientMove = inFromClient.readLine();
+            
+            
         }
     }
-
+    
     public static String JOPI(String m) {
         String r = JOptionPane.showInputDialog(null, m);
         return r;
