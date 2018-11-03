b4e67809f4d3964b4c8aac18f4551c5e3f8ec60c
Nicholas Koeppen <nicholaskoeppen@gmail.com>
2018-11-02 19:12:35 -0500

    polishing

--- RPSMultiplayer.java
+++ RPSMultiplayer.java
@@ -3,6 +3,11 @@
 import javax.swing.*;
 public class RPSMultiplayer {
     public static void main(String[] args) throws Exception {
+        Icon icon = new ImageIcon("MonaSeaver.jpg", "Mona Seaver");
+        JOptionPane.showMessageDialog(null,
+            "Insert Directions here...",
+            "Multiplayer Rock, Paper, Scissors",
+            JOptionPane.INFORMATION_MESSAGE, icon);
         String[] options = {"Host a Game", "Join a Game"};
         int r = JOptionPane.showOptionDialog(null, "Would you like to host or join the game?", "Whaddya want to do?",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
