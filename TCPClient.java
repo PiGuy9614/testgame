4c9df390749d4eae5f2212d7efda6bed3aaff8eb
Nicholas Koeppen <nicholaskoeppen@gmail.com>
2018-11-04 13:14:01 -0600

    attempt to fix mistakes? perhaps change back to hostname if mistakes were what held it back...

--- TCPClient.java
+++ TCPClient.java
@@ -7,13 +7,13 @@ class TCPClient {
     public static void main() throws Exception {
         boolean linkEstablished = false; 
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); //Gives the ability to read the user's sentence
-        String preliminaryIP = JOPI("What is the Server's IP?");
-        InetAddress serverIP = InetAddress.getByName(preliminaryIP); //Gets my IP and stores it
-        linkEstablished = serverIP.isReachable(500); //Checks if I can be connected to other PC
+        String preliminaryIP = JOPI("What is the Server's Hostname?");
+        InetAddress serverIP = InetAddress.getByName(preliminaryIP + ".local"); //Gets my IP and stores it
+        linkEstablished = serverIP.isReachable(3000); //Checks if I can be connected to other PC
         while (!linkEstablished) { //If I'm unavailable, this will explain it
-            preliminaryIP = JOPI("Please retype the Server's IP?");
+            preliminaryIP = JOPI("Please retype the Server's Hostname?");
             serverIP = InetAddress.getByName(preliminaryIP + ".local"); //Gets my IP and stores it
-            linkEstablished = serverIP.isReachable(500); //Checks if I can be connected to other PC
+            linkEstablished = serverIP.isReachable(1000); //Checks if I can be connected to other PC
         }
         Socket clientSocket = new Socket(serverIP, 9614); //Uses IP to connect to my computer's server
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
--- TCPMainClient.java
+++ TCPMainClient.java
@@ -5,11 +5,11 @@ import java.net.*;
 import javax.swing.*;
 class TCPMainClient {
     public static void main() throws Exception {
-        String clientSentence;
         ServerSocket welcomeSocket = new ServerSocket(9614);
-        InetAddress localhost = InetAddress.getLocalHost();
+        InetAddress localhost = welcomeSocket.getInetAddress();
         JOPM("Your IP Address is " + localhost.getHostAddress() + "\nPlease wait a second as your opponent is connecting...");
         Socket connection = welcomeSocket.accept();
+        
         BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         DataOutputStream outToClient = new DataOutputStream(connection.getOutputStream());
         
@@ -18,37 +18,37 @@ class TCPMainClient {
         int ui = -1, oi = -1;
         char uwon; //U = The main client and O = The opposing client
         String output;
-        
+
         outToClient.writeBytes(JOPI("Enter your username...") + "\n");
         exit:
         while(true) {
             String mcm = JOPI("You are playing with " + clientName + ". What is your move?");
             switch(mcm.toLowerCase().charAt(0)) {
                 case 'r':
-                    ui = 0;
-                    break;
+                ui = 0;
+                break;
                 case 'p':
-                    ui = 1;
-                    break;
+                ui = 1;
+                break;
                 case 's':
-                    ui = 2;
-                    break;
+                ui = 2;
+                break;
                 case 'e':
-                    break exit;
+                break exit;
                 default:
-                    mcm = JOPI("Invalid entry, enter again!");
+                mcm = JOPI("Invalid entry, enter again!");
             }
             String cm = inFromClient.readLine();
             switch(cm.charAt(0)) {
                 case 'r':
-                    oi = 0;
-                    break;
+                oi = 0;
+                break;
                 case 'p':
-                    oi = 1;
-                    break;
+                oi = 1;
+                break;
                 case 's':
-                    oi = 2;
-                    break;
+                oi = 2;
+                break;
             }
             uwon = ((ui > oi && !(ui == 2 && oi == 0)) || (ui == 0 && oi == 2)) ? 'w' : (ui == oi) ? 't' : 'l';
             output = (uwon == 'w') ? "You lost!" : (uwon == 't') ? "You tied!" : "You won!";
